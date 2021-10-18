0278253280ea5a8c1e63fa26ca4ebe89b4de5aef
laramachandran <laramachandran@deloitte.com>
2021-10-18 15:02:44 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # StateImpactReportTool
+
 Generates State Impact reports for generated CVRS Add, Update and Delete files.
+
+---
+
+This tool must be run separately for generating state impact report for Add/Update/Delete files. Meaning, if you want to generate a state impact report for Update and Delete files, you will have to run the tool twice. At a time, this tool can process more than one file, meaning, if the Update records are split across multiple files, put them in a folder and point the folder in the config mentioned below and the tool would generate one state impact report for the multiple update files.
+
+• Unzip the StateReportTool.
+• Open the StateReportTool.exe.config in a notepad and edit the following:
+
+• Edit the value field (the highlighted field below) for the filePath in the config to point to the folder where the, e.g., Delete files are.
+o <add key="filePath" value="C:\TestFiles\DeleteFiles"/>
+• Edit the value field (the highlighted field below) for the excelPath in the config. This is the folder and the file name (name of your choice ) for the State Impact Report.
+o <add key ="excelPath" value ="C:\TestFiles\GeneratedExcel\StateImpact_UD.xls"/>
+
+• Double click the StateReportTool.exe.
+• Once the processing is done, the state impact report should be available in the path mentioned in the excelPath in the StateReportTool.exe.config
---
+++ StateReportTool/StateReportTool/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace StateReportTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Processing State Impact Report-----");
+
+            string filePath = ConfigurationManager.AppSettings["filePath"];           
+            
+            DataTable mainTable = new DataTable();
+            DataTable workBookTable = new DataTable();
+
+            DirectoryInfo dir = new DirectoryInfo(filePath);
+            FileInfo[] files = dir.GetFiles("*.txt");
+            if (files.Length != 0)
+            {
+                string[] lines = File.ReadAllLines(filePath + "\\" + files[0].Name);
+                //lines [0] has column names
+                //Split all the column names
+                string[] cols = lines[0].Split('\t');                
+                foreach (var colName in cols)
+                {
+                    mainTable.Columns.Add(new DataColumn(colName.ToString()));
+                }
+            }
+           
+            using (OleDbConnection conn = new OleDbConnection())
+            {
+                foreach (FileInfo file in files)
+                {
+                    DataTable dt = mainTable.Clone();
+                    string[] lines = File.ReadAllLines(filePath + "\\" + file.Name);
+                    List<string> list = new List<string>(lines);                    
+                    list.RemoveAt(0);
+                    lines = list.ToArray();
+                    foreach (string line in lines)
+                    {
+                        var cols = line.Split('\t');
+
+                        DataRow dr = dt.NewRow();
+                        for (int cIndex = 0; cIndex < cols.Length; cIndex++)
+                        {
+                            dr[cIndex] = cols[cIndex];
+                        }
+
+                        dt.Rows.Add(dr);
+                    }
+                 
+                    mainTable.Merge(dt);
+                }
+                
+                var query = from row in mainTable.AsEnumerable()
+                            group row by row.Field<string>("recip_address_state") into stateReport
+                            orderby stateReport.Count() descending
+                            select new
+                            {
+                                State = stateReport.Key,
+                                Count = stateReport.Count()
+                            };
+
+                workBookTable.Columns.Add(new DataColumn("State"));
+                workBookTable.Columns.Add(new DataColumn("Count"));
+                // create a data table for WorkBook
+                foreach (var obj in query)
+                {
+                    DataRow dr = workBookTable.NewRow();
+                    dr["State"] = obj.State;
+                    dr["Count"] = obj.Count;
+                    workBookTable.Rows.Add(dr);
+                }
+
+                ExportToExcel(workBookTable);                
+                Console.ReadKey();
+            }
+
+        }
+
+        static void ExportToExcel(DataTable dataTable)
+        {            
+            object misValue = System.Reflection.Missing.Value;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook  = xlApp.Workbooks.Add(misValue);
+            Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            for (int i = 1; i < dataTable.Columns.Count + 1; i++)
+            {
+                xlWorkSheet.Cells[1, i] = dataTable.Columns[i - 1].ColumnName;
+            }
+            for (int j = 0; j < dataTable.Rows.Count; j++)
+            {
+                for (int k = 0; k < dataTable.Columns.Count; k++)
+                {
+                    xlWorkSheet.Cells[j + 2, k + 1] = dataTable.Rows[j].ItemArray[k].ToString();
+                }
+            }          
+
+            xlWorkBook.SaveAs(ConfigurationManager.AppSettings["excelPath"], Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+            Console.WriteLine("State Impact report generated at " +  ConfigurationManager.AppSettings["excelPath"]);
+        }
+
+        static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+               Console.WriteLine("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
